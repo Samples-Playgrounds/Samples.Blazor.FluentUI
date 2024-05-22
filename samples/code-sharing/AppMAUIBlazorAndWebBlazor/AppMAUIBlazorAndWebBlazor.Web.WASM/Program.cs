52f341a2833b746a3482010c73992c90383cb40e
moljac <mcvjetko@holisticware.net>
2024-05-22 06:06:13 +0200

    WASM modificartions to use shared

--- samples/code-sharing/AppMAUIBlazorAndWebBlazor/AppMAUIBlazorAndWebBlazor.Web.WASM/Program.cs
+++ samples/code-sharing/AppMAUIBlazorAndWebBlazor/AppMAUIBlazorAndWebBlazor.Web.WASM/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using AppMAUIBlazorAndWebBlazor.Web.WASM;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+using AppMAUIBlazorAndWebBlazor.Web.WASM.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
