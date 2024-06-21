fb6d232cffdeb0f543fd0095051dd1753ba6f04c
moljac <mcvjetko@holisticware.net>
2024-06-21 21:13:57 +0200

    sample01 and sample02 tested by aspire

--- samples/code-sharing/Sample.AppAspire/Sample.AppAspire.AppHost/Program.cs
+++ samples/code-sharing/Sample.AppAspire/Sample.AppAspire.AppHost/Program.cs
@@ -2,7 +2,8 @@ using Projects;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Sample01_Blazor_MAUI_Hybrid_Web_Bootstrap_Web>("app-web");
+builder.AddProject<Sample01_Blazor_MAUI_Hybrid_Web_Bootstrap_Web>("app-web-bootstrap");
+builder.AddProject<Sample02_Blazor_MAUI_Hybrid_Web_FluentUI_Web>("app-web-fluentui");
 
 /*
     MAUI setup
