f8bba3766a277715e9bc560c5c97b683b72d11ee
moljac <mcvjetko@holisticware.net>
2024-06-29 19:27:55 +0200

    asire fixed for new projects

--- samples/code-sharing/Sample.AppAspire/Sample.AppAspire.AppHost/Program.cs
+++ samples/code-sharing/Sample.AppAspire/Sample.AppAspire.AppHost/Program.cs
@@ -3,7 +3,7 @@ using Projects;
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Sample01_Blazor_MAUI_Hybrid_Web_Bootstrap_Web>("app-web-bootstrap");
-builder.AddProject<Sample02_Blazor_MAUI_Hybrid_Web_FluentUI_Web>("app-web-fluentui");
+builder.AddProject<Sample_Blazor_MAUI_Hybrid_Web_FluentUI_Web>("app-web-fluentui");
 
 /*
     MAUI setup
