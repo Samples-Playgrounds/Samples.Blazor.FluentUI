e8062067a31a6512385ce6407949bd5ed2b396d3
moljac <mcvjetko@holisticware.net>
2024-08-18 10:50:59 +0200

    moved to externals

---
+++ samples/code-sharing/externals/download-samples.sh
@@ -0,0 +1,48 @@
+
+export OWNER=BethMassi
+export REPO=BethTimeUntil
+export BRANCH=master
+export URL=https://github.com/$OWNER/$REPO/archive/refs/heads/$BRANCH.zip
+curl -v -L -C - -O $URL
+mv $BRANCH.zip $OWNER.$REPO.zip
+unzip $OWNER.$REPO.zip
+
+export OWNER=iceHub82
+export REPO=IceNet.Shared
+export BRANCH=master
+export URL=https://github.com/$OWNER/$REPO/archive/refs/heads/$BRANCH.zip
+curl -v -L -C - -O $URL
+mv $BRANCH.zip $OWNER.$REPO.zip
+unzip $OWNER.$REPO.zip
+
+export OWNER=Eilon
+export REPO=MauiHybridWebView
+export BRANCH=main
+export URL=https://github.com/$OWNER/$REPO/archive/refs/heads/$BRANCH.zip
+curl -v -L -C - -O $URL
+mv $BRANCH.zip $OWNER.$REPO.zip
+unzip $OWNER.$REPO.zip
+
+export OWNER=Eilon
+export REPO=SampleMauiHybridWebViewProject
+export BRANCH=main
+export URL=https://github.com/$OWNER/$REPO/archive/refs/heads/$BRANCH.zip
+curl -v -L -C - -O $URL
+mv $BRANCH.zip $OWNER.$REPO.zip
+unzip $OWNER.$REPO.zip
+
+export OWNER=BethMassi
+export REPO=HybridSharedUI
+export BRANCH=master
+export URL=https://github.com/$OWNER/$REPO/archive/refs/heads/$BRANCH.zip
+curl -v -L -C - -O $URL
+mv $BRANCH.zip $OWNER.$REPO.zip
+unzip $OWNER.$REPO.zip
+
+export OWNER=kurthildebrand
+export REPO=HybridWebView
+export BRANCH=main
+export URL=https://github.com/$OWNER/$REPO/archive/refs/heads/$BRANCH.zip
+curl -v -L -C - -O $URL
+mv $BRANCH.zip $OWNER.$REPO.zip
+unzip $OWNER.$REPO.zip
